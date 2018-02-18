756b1bff2b25eff22180ce38797b06299b531885
Matthew T. Gilbride <mgilbride@chariotsolutions.com>
2018-02-17 19:16:10 -0500

    handle the mouseout events on the menu

--- src/MenuComponent.js
+++ src/MenuComponent.js
@@ -1,32 +1,48 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Dropdown, Menu } from 'semantic-ui-react';
+
+class DocsDropdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      docs: false,
+      tgraph: false,
+      examples: false,
+    }
+  }
+  render() {
+    return (
+      <Dropdown basic simple item text="Docs" open={this.state.docs}>
+        <Dropdown.Menu>
+          <Dropdown.Item as="a" href="#/about" text="About" onMouseLeave={() => this.setState({ ...this.state, docs: false })} />
+          <Dropdown compact basic simple item text="TGraph" open={this.state.tgraph} onMouseLeave={() => this.setState({ ...this.state, docs: false })}>
+            <Dropdown.Menu>
+              <Dropdown.Item as="a" href="#/tgraph" text="Overview" onMouseLeave={() => this.setState({ ...this.state, tgraph: false })}/>
+              <Dropdown.Item as="a" href="#/representations" text="Representations" onMouseLeave={() => this.setState({ ...this.state, tgraph: false })}/>
+              <Dropdown.Item as="a" href="#/operators" text="Operators" onMouseLeave={() => this.setState({ ...this.state, tgraph: false })}/>
+              <Dropdown.Item as="a" href="#/analytics" text="Analytics" onMouseLeave={() => this.setState({ ...this.state, tgraph: false })}/>
+            </Dropdown.Menu>
+          </Dropdown>
+          <Dropdown.Item as="a" href="https://portaldb.github.io/api" text="API" onMouseLeave={() => this.setState({ ...this.state, docs: false })} />
+          <Dropdown compact basic simple item text="Examples" open={this.state.examples} onMouseLeave={() => this.setState({ ...this.state, docs: false })} >
+            <Dropdown.Menu>
+              <Dropdown.Item as="a" href="#/examples" text="Overview" onMouseLeave={() => this.setState({ ...this.state, examples: false })}/>
+              <Dropdown.Item as="a" href="#/vertexInfluenceOverTime" text="Vertex Influence" onMouseLeave={() => this.setState({ ...this.state, examples: false })}/>
+              <Dropdown.Item as="a" href="#/graphCentralityOverTime" text="Graph Centrality" onMouseLeave={() => this.setState({ ...this.state, examples: false })}/>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Dropdown.Menu>
+      </Dropdown>
+    )
+  }
+}
 
 const MenuComponent = ({children, history}) => (
   <Container>
     <Menu borderless fluid color="black" inverted widths="4">
       <Menu.Item as="a" href="#/">Home</Menu.Item>
-      <Dropdown basic simple item text="Docs">
-        <Dropdown.Menu>
-          <Dropdown.Item as="a" href="#/about" text="About" />
-          <Dropdown compact basic simple item text="TGraph">
-            <Dropdown.Menu>
-              <Dropdown.Item as="a" href="#/tgraph" text="Overview" />
-              <Dropdown.Item as="a" href="#/representations" text="Representations" />
-              <Dropdown.Item as="a" href="#/operators" text="Operators" />
-              <Dropdown.Item as="a" href="#/analytics" text="Analytics" />
-            </Dropdown.Menu>
-          </Dropdown>
-          <Dropdown.Item as="a" href="https://portaldb.github.io/api" text="API" />
-          <Dropdown compact basic simple item text="Examples" >
-            <Dropdown.Menu>
-              <Dropdown.Item as="a" href="#/examples" text="Overview" />
-              <Dropdown.Item as="a" href="#/vertexInfluenceOverTime" text="Vertex Influence" />
-              <Dropdown.Item as="a" href="#/graphCentralityOverTime" text="Graph Centrality" />
-            </Dropdown.Menu>
-          </Dropdown>
-        </Dropdown.Menu>
-      </Dropdown>
+      <DocsDropdown/>
       <Menu.Item as="a" href="#/installation">Install</Menu.Item>
       <Menu.Item as="a" href="#/publications">Publications</Menu.Item>
     </Menu>
