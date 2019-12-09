c0ac7b9938d7571024c70ca91cce45220cc6e52d
Amir Pouya Aghasadeghi <amirpouya.a@gmail.com>
2019-12-09 10:50:52 -0500

    Update PublicationsComponent.js

--- src/PublicationsComponent.js
+++ src/PublicationsComponent.js
@@ -8,9 +8,6 @@ const PublicationsComponent = () => (
       Publications
     </Header>
     <Segment>
-      <Container>
-        Portal is a result of research conducted at the <a href="https://www.cs.drexel.edu/dbgroup/">Drexel Database Group</a>.
-      </Container>
       <ul>
         <li><a href="https://dl.acm.org/citation.cfm?doid=3122831.3122838">Temporal graph algebra</a></li>
         <li><a href="http://openproceedings.org/2017/conf/edbt/paper-274.pdf">Towards sequenced semantics for evolving graph</a></li>
