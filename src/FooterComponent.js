e649cc13de9277300fb42a215e985f771ef6c87f
Amir Pouya Aghasadeghi <amirpouya.a@gmail.com>
2020-04-20 19:06:39 -0400

    Update FooterComponent.js

--- src/FooterComponent.js
+++ src/FooterComponent.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-function Footer() {
+class Footer extends Component{
+  render() {
   return (
     <div>
       <h1>This research is supported by <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1750179"> NSF CAREER 1750179 </a>  "Querying Evolving Graphs".
 </h1>
     </div>
   );
+}
 }
 
 export default Footer;
