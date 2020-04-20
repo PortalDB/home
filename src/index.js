76ef708874f305810159b14f6dc96d6d36ea7219
Amir Pouya Aghasadeghi <amirpouya.a@gmail.com>
2020-04-20 19:16:59 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,6 @@ import Footer from './Footer';
 import './App.css'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'), 
-  <div>
-      <h1>This research is supported by <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1750179"> NSF CAREER 1750179 </a>  "Querying Evolving Graphs".
-</h1>
-    </div>
-  
+ReactDOM.render(<App />, document.getElementById('root')
   );
 registerServiceWorker();
